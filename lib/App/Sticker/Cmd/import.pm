734341db9e20230a89b3f6f69e086aee84f4a8f0
Mario Domgoergen <mdom@taz.de>
2015-06-04 22:33:37 +0200

    also import date_added

--- lib/App/Sticker/Cmd/import.pm
+++ lib/App/Sticker/Cmd/import.pm
@@ -9,16 +9,27 @@ with( 'App::Sticker::UA', 'App::Sticker::Util', 'App::Sticker::Modifier' );
 sub execute {
     my $self  = shift;
     my @files = @ARGV;
-    my @all_urls;
+    my @attrs;
     for my $file (@files) {
         my $content = path($file)->slurp_utf8;
         my $dom     = Mojo::DOM->new($content);
-        my $urls =
-          $dom->find('a["href"]')->map( attr => 'href' )
-          ->grep( sub { /^http/ } )->to_array;
-        push @all_urls, @$urls;
+        my $attrs   = $dom->find('a["href"]')->map('attr')
+          ->grep( sub { $_->{href} =~ /^http/ } )->to_array;
+	push @attrs, @$attrs;
     }
-    return $self->add_urls( \@all_urls );
+    my %attrs = map { $self->normalize_url( $_->{href} ) => $_ } @attrs;
+    my @urls = keys %attrs;
+
+    my $urls_added = $self->add_urls( \@urls );
+
+    my $db         = $self->base->db;
+    for my $url ( @$urls_added ) {
+        my $doc = $db->get($url);
+	next unless exists $attrs{$url}->{date_added};
+        $doc->{date_added} = $attrs{$url}->{date_added};
+	$db->set($doc);
+    }
+    return;
 }
 
 1;
