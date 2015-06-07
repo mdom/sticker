3bc9b126c723ef0b97ebc283c3a4f7b2530c201d
Mario Domgoergen <mdom@taz.de>
2015-06-07 21:46:50 +0200

    expand urls with commas and ranges

--- lib/App/Sticker/Util.pm
+++ lib/App/Sticker/Util.pm
@@ -9,17 +9,34 @@ sub to_url {
     my ( $self, @urls ) = @_;
     my @normalized_urls;
     for my $url (@urls) {
-        if ( $url =~ /^\d+$/ ) {
+        if ( $url =~ /^\d+/ ) {
             my $last_search = $self->base->base_dir->child('last_search');
             if ( $last_search->exists ) {
                 my @urls = $last_search->lines( { chomp => 1 } );
-                $url = $urls[ $url - 1 ];
+                my @nums = map { $_ - 1 } $self->_expand_nums($url);
+                push @normalized_urls, @urls[@nums];
             }
         }
-        $url = $self->normalize_url($url);
-        push @normalized_urls, $url;
+        else {
+            $url = $self->normalize_url($url);
+            push @normalized_urls, $url;
+        }
     }
     return @normalized_urls;
+}
+
+sub _expand_nums {
+    my ( $self, $num ) = @_;
+    my @nums;
+    for my $num ( split( ',', $num ) ) {
+        if ( $num =~ /^(\d+)-(\d+)$/) {
+            push @nums, $1 .. $2;
+        }
+        else {
+            push @nums, $num;
+        }
+    }
+    return @nums;
 }
 
 sub normalize_url {
